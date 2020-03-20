d9745a54351da8697e3173da24502e1b4385e200
Ping Zhao <pzhao11@gmail.com>
2020-03-20 16:03:16 -0700

    Grading Exercise complete

--- 2020-03-23-856GradingEx-Ping-Zhao/ViewController.swift
+++ 2020-03-23-856GradingEx-Ping-Zhao/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var gradeInput: UITextField!
+    
+    @IBOutlet weak var gradeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func gradeButtonClicked(_ sender: Any) {
+        
+        var gradeNum = Int(gradeInput.text!)!
+        var letter = ""
+        
+        if (gradeNum >= 93) {
+            letter = "A"
+        }
+        else if (gradeNum >= 90) {
+            letter = "A-"
+        }
+        else if (gradeNum >= 87) {
+            letter = "B+"
+        }
+        else if (gradeNum >= 83) {
+            letter = "B"
+        }
+        else if (gradeNum >= 80) {
+            letter = "B-"
+        }
+        else if (gradeNum >= 77) {
+            letter = "C+"
+        }
+        else if (gradeNum >= 73) {
+            letter = "C"
+        }
+        else if (gradeNum >= 70) {
+            letter = "C-"
+        }
+        else if (gradeNum >= 67) {
+            letter = "D+"
+        }
+        else if (gradeNum >= 63) {
+            letter = "D"
+        }
+        else if (gradeNum >= 60) {
+            letter = "D-"
+        }
+        else {
+            letter = "F"
+        }
+        gradeLabel.text = "Letter Grade: \(letter)"
+    }
+    
 }
 
